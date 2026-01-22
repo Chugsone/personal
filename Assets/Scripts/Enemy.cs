5256165ff22b09807676da3e1d17fed7bd721d13
MUKILTEO\1726377 <chasebannavong@gmail.com>
2026-01-22 11:22:13 -0800

    abra cadabra

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -82,7 +82,7 @@ public class Enemy : MonoBehaviour
                 Player player = other.GetComponent<Player>();
                 if (player != null)
                 {
-                    player.TakeDamage(contactDamage);
+                    object damageTaken = player.TakeDamage(contactDamage);
                     lastAttackTime = Time.time;
                 }
                 else
