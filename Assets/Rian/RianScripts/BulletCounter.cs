a9c65e09ab2dcf38ebcd99acb4c1daa12d8a6f19
MUKILTEO\1726409 <r.francis.yeet@outlook.com>
2026-02-26 12:32:18 -0800

    added bullet counter and changed bullet textire

---
+++ Assets/Rian/RianScripts/BulletCounter.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+public class BulletCounter : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+    public TMPro.TMP_Text Ammo;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ammo.SetText($"{playerMovement.bullets}|{playerMovement.mag}"); 
+    }
+}
